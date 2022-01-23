9f8879143587f2edfd6128844d4798209b413c14
PaolaCasillo <879931@stud.unive.it>
2022-01-23 14:39:15 +0100

    codinc Check class

--- checking.py
+++ checking.py
@@ -7,3 +7,16 @@ the drink to know whether they are already
 present in the "cocktails_data.csv file" respectively.
 """
 
+import pandas as pd
+
+
+class Check:
+
+    def check_artist(self, artist):
+        
+        artist = artist.lower()
+        db = pd.DataFrame(pd.read_csv('cocktails_data.csv'))
+        artists = db["Drink"].str.lower()
+        if artist in artists.values:
+            return True
+        return False
