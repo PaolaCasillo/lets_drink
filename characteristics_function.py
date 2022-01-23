75736d510337b302cf8462b47d67e14f7191660e
RedTaylorsVersion <you@example.com>
2022-01-23 17:22:57 +0100

    ideation of charecteristics funcion

---
+++ characteristics_function.py
@@ -0,0 +1,17 @@
+"""
+The function of the characteristcs module
+is to explain in a more detailed way the characteristics,
+like the category, ingridiets, taste, instruction and
+glass to be serve with, of the cocktails
+that can be found into our database.
+The function first checks if the input
+the user inserted is present in
+the database, if not the system will warn the user
+and invite him to check if it is correctly written.
+"""
+
+import csv
+import pandas as pd
+
+
+
