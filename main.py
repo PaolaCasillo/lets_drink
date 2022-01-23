3e06f2a1c564c8de67922e91f4b85ece313058d5
PaolaCasillo <879931@stud.unive.it>
2022-01-23 13:32:09 +0100

    main setup

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+This is the main file where we created
+the interaction between the user and the
+database of drinks.
+We used the argparse module to configure
+all the positional and optional arguments.
+Depending on the arguments given,
+the program leads the user to a series of steps
+to either check the presence of the cocktail imputed
+inside the database and/or add it.
+There is also the possibility to check out
+the association between cocktails and alchol type.
+"""
