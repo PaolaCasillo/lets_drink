847c84e5848063190f39846076d786cf8e7d9751
PaolaCasillo <879931@stud.unive.it>
2022-01-23 14:36:01 +0100

    definition of checker function

---
+++ checking.py
@@ -0,0 +1,9 @@
+"""
+This module focuses on checking if the input
+given by the user is present in our database (csv file)
+that contains information of cocktails.
+It contains a functions where the user inputs the name of
+the drink to know whether they are already
+present in the "cocktails_data.csv file" respectively.
+"""
+
