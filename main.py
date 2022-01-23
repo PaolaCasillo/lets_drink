b140c8dba54f514c3cba62fbb6b39b8df418f454
RedTaylorsVersion <you@example.com>
2022-01-23 17:25:54 +0100

    solved problem

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 This is the main file where we created
-the interaction between the user and the
-database of drinks.
+the interaction between the user and
+the database of drinks.
 We used the argparse module to configure
 all the positional and optional arguments.
 Depending on the arguments given,
