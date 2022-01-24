fe8c6d1781fa80cebaddb63f7053bb9b44526410
RedTaylorsVersion <you@example.com>
2022-01-24 22:02:38 +0100

    adding variables

--- inserter.py
+++ inserter.py
@@ -10,6 +10,39 @@ of our database.
 """
             
 
-
+def add_element(drink, response=""):
 
             
+ db = pd.DataFrame(pd.read_csv('cocktails_data.csv'))
+
+    ingridients = input(
+        "Now enter the ingridients of the drink->")
+    while(ingridients == ""):
+        ingridients = input(
+            "You can't enter nothing..." + "please put anything ->")
+
+    instruction = input("Now enter the intruction to do it -> ")
+    while(instruction == ""):
+        instruction = input("You can't enter nothing... " +
+
+                                  "so please... put anything -> ")
+    Alcohol_type = input(
+        "Now enter the Alcohol_type -> ")
+    while(Alcohol_type == ""):
+        Alcohol_type = input(
+            "You can't enter nothing..." + "please put anything ->")
+
+    glass = input("Now enter the type of glass to use to serve it->")
+    while(glass == ""):
+        glass = input("You can't enter nothing..." + "please put anything ->")
+
+    Basic_taste = input("Now enter the basic taste of the drink->")
+    while(Basic_taste == ""):
+        Basic_taste = input("You can't enter nothing..." + "please put anything ->")
+                
+    category = input(
+        "Now enter the category of the drink->")
+    while(category == ""):
+        category = input(
+            "You can't enter nothing..." + "please put anything ->")
+
