cc7963eb69a44e88bd8e80061c7198a06c56f42c
RedTaylorsVersion <you@example.com>
2022-01-24 23:01:55 +0100

    pushing description

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Depending on the arguments given,
 the program leads the user to a series of steps
 to either check the presence of the cocktail imputed
 inside the database and/or add it.
+
 There is also the possibility to check out
 the association between cocktails and alchol type.
 """
