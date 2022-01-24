15b26bdf2630d92b44a81d97a7045caafdbaf4c0
RedTaylorsVersion <you@example.com>
2022-01-24 21:58:37 +0100

    creation of inserter

---
+++ inserter.py
@@ -0,0 +1,15 @@
+"""
+The function of the inserter module
+allows to append new drink into our database.
+The function requires to add all the
+other additional information, to generate
+a list of 8 items.
+Finally the list is inserted in a new
+row becoming part
+of our database.
+"""
+            
+
+
+
+            
